a78732ee024463d17baf03efcc8110fec78f4fd4
Mr.Void <adrytimus@gmail.com>
2026-04-29 22:41:06 +0300

    Add database connection using EF Core

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using Management_Internet_Cafe.Data;
+
 namespace Management_Internet_Cafe
 {
   public partial class Form1 : Form
@@ -5,6 +7,22 @@ namespace Management_Internet_Cafe
     public Form1()
     {
       InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      try
+      {
+        using (var db = new AppDbContext())
+        {
+          bool canConnect = db.Database.CanConnect();
+          MessageBox.Show(canConnect ? "Database connected!" : "Connection failed!");
+        }
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show("Error: " + ex.Message);
+      }
     }
   }
 }
---
+++ Models/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Management_Internet_Cafe.Data
+{
+  public class AppDbContext : DbContext
+  {
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+      options.UseSqlServer(
+          @"Server=.\SQLEXPRESS;Database=Internet_Cafe_DB;Trusted_Connection=True;TrustServerCertificate=True;");
+    }
+  }
+}
